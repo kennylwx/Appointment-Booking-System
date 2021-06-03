389dcee772a8608f7954f97d594fa7c8d96913e0
Kenny Lee <kenny_seven@icloud.com>
2021-06-03 13:57:13 +1000

    restructuring files

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/header.scss';
+
+function header() {
+  return (
+    <header className="header">
+      header.
+    </header>
+  );
+}
+
+export default header;
---
+++ src/views/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.scss';
+import Header from '../components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      This is the main app.
+    </div>
+  );
+}
+
+export default App;
